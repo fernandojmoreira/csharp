7a3d5afbde5a0ee34dee912ceb5dbc0f2823fa0f
Fernando Moreira <84102190+fernandojmoreira@users.noreply.github.com>
2022-11-18 18:25:00 -0300

    Adicionar arquivos de projeto.

---
+++ LacosIterativos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacosIterativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Laços iterativos => estruturas que repetem um bloco de código determinado número de vezes
+
+            // for
+            for(int i=0; i<5; i++) // i++ significa que i será incrimentado de 1 em 1
+            {
+                Console.WriteLine("Valor de i é " + i);
+            }
+
+            // while
+            int contador = 3;
+            while(contador < 10)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+
+            // do .. while
+            // execução garantida pelo menos uma vez (executa o bloco primeiro e depois faz o teste de condição)
+            double j = 10;
+            do
+            {
+                Console.WriteLine(j);
+                j = j * 1.5;
+            } while (j < 100);
+
+            // foreach
+            // percorre todos os elementos de um conjunto
+            int[] conjunto = { 1, 2, 3, 8, 13 };
+            foreach(int numero in conjunto)
+            {
+                Console.WriteLine("Esse elemento do conjunto tem valor " + numero + "! Somando esse valor a 10 temos " + (numero + 10));
+            }
+        }
+    }
+}
---
+++ OláMundo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Pasta lógica
+namespace OlaMundo
+{
+    class Program
+    {
+        // Método Main:
+        // Ponto de entrada para a aplicação;
+        // O que define o início da aplicação
+        static void Main(string[] args)
+        {
+            // Imprimir na tela o famoso "Olá Mundo !"
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Meu nome é Fernando");
+        }
+    }
+}
